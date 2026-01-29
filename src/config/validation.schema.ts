3e8a3e565e2b802285872ad743af35b341ddef7c
tnh-jonghwan <jongdeug@trustnhope.com>
2026-01-29 15:43:10 +0900

    [chore]: contactId 추가

--- src/config/validation.schema.ts
+++ src/config/validation.schema.ts
@@ -1,10 +1,10 @@
 import * as Joi from 'joi';
 
 export const validationSchema = Joi.object({
-  // respond.io
+  // Respond.io
   RESPOND_IO_API_KEY: Joi.string().required(),
   
-  // Polling (Growth plan)
+  // 폴링 방식
   POLLING_ENABLED: Joi.string().valid('true', 'false').default('false'),
   POLLING_CONTACTS: Joi.string().allow('').default(''), // comma-separated contact IDs
   
